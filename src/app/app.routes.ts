8e2ef2251453d007d583ccf9e3510a245491598c
Eduard0w <dudusouza5595@gmail.com>
2025-11-29 17:55:10 -0300

    Criação da tela criar conta completa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { CreatAcountComponent } from './pages/creat-acount/creat-acount.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Rota coringa para URLs não encontradas
   { path: 'login', component: LoginComponent },
+  { path: 'creatAcount', component: CreatAcountComponent },
   { path: '**', component: HomeComponent },
 ];
 
---
+++ src/app/pages/creat-acount/creat-acount.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creat-acount',
+  imports: [],
+  templateUrl: './creat-acount.component.html',
+  styleUrl: './creat-acount.component.css'
+})
+export class CreatAcountComponent {
+
+}
