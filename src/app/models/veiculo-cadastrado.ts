a695059e76fbecb251afc43ff0a5dd073ae86f4c
Eduard0w <dudusouza5595@gmail.com>
2025-12-02 08:09:07 -0300

    arquivo veiculo-cadastrado.ts completo com sua interface

--- src/app/models/veiculo-cadastrado.ts
+++ src/app/models/veiculo-cadastrado.ts
@@ -1,2 +1,17 @@
+import { Data } from '@angular/router';
+
 export interface VeiculoCadastrado {
+  marca: string;
+  modelo: string;
+  ano: number;
+  placa?: string;
+  combustivel: string;
+  quilometragem: number;
+  // Data da ultima troca + KM da ultima troca
+  ult_troca_oleo: [Data, number];
+  ult_troca_filtro: [Data, number];
+  ult_troca_pastilhas: [Data, number];
+  alerta_painel: string[];
+  tipo_uso: string;
+  KM_medio_p_dia: number;
 }
