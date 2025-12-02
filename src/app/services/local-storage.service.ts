45b7dbb3ff34ac4dfc771ec80f7de7a9806e6718
Eduard0w <dudusouza5595@gmail.com>
2025-12-02 07:58:43 -0300

    localStorage service - metodo já implementados, novo arquivo veiculo-cadastrado - uma interface

--- src/app/services/local-storage.service.ts
+++ src/app/services/local-storage.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LocalStorageService {
+  set<T>(key: string, value: T): void {
+    const json = JSON.stringify(value);
+    localStorage.setItem(key, json);
+  }
 
-  constructor() { }
+  get<T>(key: string): T | null {
+    const data = localStorage.getItem(key);
+    return data ? JSON.parse(data) : null;
+  }
+
+  remove(key: string): void {
+    localStorage.removeItem(key);
+  }
+
+  clear(): void {
+    localStorage.clear();
+  }
+  constructor() {}
 }
